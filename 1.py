de8a98799b736fc9358df63decca13561de7f0ea
Alexandr Dulovec <alexandr.dulovec@student.ossp.cz>
2023-09-26 14:05:41 +0200

    259

---
+++ 1.py
@@ -0,0 +1,6 @@
+keys = ["John", "Marie", "Alex"]
+values = ["41", "15", "66"]
+
+slovnik = dict(zip(keys, values))
+
+print(slovnik)
---
+++ 2.py
@@ -0,0 +1,9 @@
+trida = {}
+
+trida["student1"] = {"jméno": "Alexandr", "věk": 19}
+trida["student2"] = {"jméno": "Matěj", "věk": 19}
+trida["student3"] = {"jméno": "Martin", "věk": 18}
+trida["student4"] = {"jméno": "Lukáš", "věk": 19}
+trida["student5"] = {"jméno": "Honza", "věk": 18}
+
+print(trida)
